b7aff0c773ec6bf3fe1e08781f31762d2b9e388e
ducmanhchy <ducmanhchy@gmail.com>
2018-12-26 13:28:23 +0700

    update base web core

---
+++ ManagerUser/ManagerUser/ManagerUsers/Action.ashx.cs
@@ -0,0 +1,72 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace ManagerUser
+{
+    /// <summary>
+    /// Summary description for Action
+    /// </summary>
+    public class Action : pActionBase
+    {
+        ResultAction oResult = new ResultAction();
+        public IMongoCollection<UserEntity> users { get; private set; }
+        public IMongoCollection<UsersBson> Users { get; private set; }
+        UserEntity user = new UserEntity();
+        UsersBson User = new UsersBson();
+        public UsersDA UsersDA = new UsersDA();
+        public override void ProcessRequest(HttpContext context)
+        {
+            base.ProcessRequest(context);
+            switch (DoAction.ToUpper())
+            {
+                case "QUERYDATA":
+                    users = UsersDA.GetCollection();
+                    var result2 = users.Find(FilterDefinition<UserEntity>.Empty).ToListAsync().GetAwaiter().GetResult();
+                    var jsonWriterSettings = new JsonWriterSettings { OutputMode = JsonOutputMode.Strict };
+                    oResult.OData = result2.ToJson(jsonWriterSettings);
+                    break;
+                case "UPDATE":
+                    Users = UsersDA.GetCollectionBson();
+                    User.UpdateObject(context.Request);
+                    if (!ObjectId.TryParse(_id, out ObjectId objectid))
+                        objectid = ObjectId.Empty;
+                    if (objectid != ObjectId.Empty)
+                    {
+                        var updateFilter = Builders<UsersBson>.Filter.Eq("_id", BsonObjectId.Create(objectid));
+                        var updateValues = new List<UpdateDefinition<UsersBson>>();
+                        if (!string.IsNullOrEmpty(User.Name)) { updateValues.Add(Builders<UsersBson>.Update.Set("Name", User.Name)); }
+                        if (!string.IsNullOrEmpty(User.LoginName)) { updateValues.Add(Builders<UsersBson>.Update.Set("LoginName", User.LoginName)); }
+                        if (!string.IsNullOrEmpty(User.PassWord)) { updateValues.Add(Builders<UsersBson>.Update.Set("PassWord", User.PassWord)); }
+                        if (!string.IsNullOrEmpty(User.DateOfBirth)) { updateValues.Add(Builders<UsersBson>.Update.Set("DateOfBirth", User.DateOfBirth)); }
+                        if (Convert.ToInt32(User.Gender) > 0) { updateValues.Add(Builders<UsersBson>.Update.Set("Gender", User.Gender)); }
+                        if (!string.IsNullOrEmpty(User.LastLogin)) { updateValues.Add(Builders<UsersBson>.Update.Set("LastLogin", User.LastLogin)); }
+                        UpdateDefinition<UsersBson> update = Builders<UsersBson>.Update.Combine(updateValues);
+                        Users.UpdateOne(updateFilter, update);
+                    }
+                    else
+                    {
+                        Users.InsertOneAsync(User);
+                    }
+                    oResult.Message = "Lưu thành công";
+                    break;
+                case "DELETE":
+                    {
+                        string outb = UsersDA.DeleteUserById(_id);
+                        if (string.IsNullOrEmpty(outb))
+                            oResult.Message = "Xóa thành công";
+                        else oResult.Message = outb;
+                        break;
+                    }
+                case "DELETE-MULTI":
+                    break;
+            }
+            oResult.ResponseData();
+        }
+    }
+}
---
+++ ManagerUser/ManagerUser/ManagerUsers/ViewUser.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewUser.aspx.cs" Inherits="ManagerUser.ViewUser" %>
+
+<div id="form_User">
+    <form class="box_view form-horizontal">
+        <div class="form-group">
+            <label for="Name" class="col-xs-2 control-label">Tên người dùng</label>
+            <div class="col-xs-4">
+                <p class="form-control-static"><%:oUser.Name %></p>
+            </div>
+            <label for="LoginName" class="col-xs-2 control-label">Tên đăng nhập</label>
+            <div class="col-xs-4">
+                <p class="form-control-static"><%:oUser.LoginName %></p>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="DateOfBirth" class="col-xs-2 control-label">Ngày sinh</label>
+            <div class="col-xs-4">
+                <p class="form-control-static"></p>
+            </div>
+            <label for="Gender" class="col-xs-2 control-label">Giới tính</label>
+            <div class="col-xs-4">
+                <p class="form-control-static"><%:oUser.Gender == 1 ? "Nam" : "Nữ" %></p>
+            </div>
+        </div>
+        <div class="form-group" id="LastLogin">
+            <label for="LastLogin" class="col-xs-2 control-label">đăng nhập lần cuối</label>
+            <div class="col-xs-4">
+            </div>
+            <label for="LastLocal" class="col-xs-2 control-label">vị trí cuối cùng</label>
+            <div class="col-xs-4">
+                <p class="form-control-static"><%:oUser.LastLocal %></p>
+            </div>
+        </div>
+        <div class="form-group text-right">
+            <div class="col-xs-12">
+                <button class="btn btn-default btn-huy" type="button">Đóng</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $(".btn-huy").click(function () {
+            $(this).closefrmbs();
+        });
+    });
+</script>
